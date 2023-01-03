1989e453cec4565bb3c5a3d4d23b56ec5395b74b
yoichi0615 <aaa11655pt@gmail.com>
2023-01-03 12:50:44 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const [num, setNum] = useState<number>(0)
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8001/api/list')
+      .then(res => {
+        setNum(res.data.id)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>APP</div>
+      <div>{num}</div>
+    </>
+  )
 }
 
 export default App;
